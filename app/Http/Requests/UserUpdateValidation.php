f999ce8df43b7902c5f79b67e81e5f84ca7d6652
cdvillamil92 <cvillamil@dayscript.com>
2018-09-14 10:50:41 -0500

    Validaciones

--- app/Http/Requests/UserUpdateValidation.php
+++ app/Http/Requests/UserUpdateValidation.php
@@ -12,6 +12,7 @@ class UserUpdateValidation extends FormRequest
      *
      * @return bool
      */
+
     public function authorize()
     {
          //Auth::user()->hasRole('Admin');
@@ -23,29 +24,34 @@ class UserUpdateValidation extends FormRequest
      *
      * @return array
      */
+
     public function rules()
     {
 
         return [
             'first_name' => 'required',
             'last_name' => 'required',
-            'identification' => 'required',
+            //'identification' => 'required|unique',
             'city' => 'required',
             'affiliate_time' => 'required',
-
+            'email' => 'required|email',
+            'phone' => 'required',
+            'mobile' => 'required',
         ];
     }
-
 
     public function messages()
     {
         return [
             'first_name.required' => 'El campo Nombres es obligatorio.',
             'last_name.required' => 'El campo Apellidos es obligatorio.',
-            'identification.required' => 'El campo No Documento es obligatorio.',
+            //'identification.required' => 'El campo No Documento es obligatorio.',
             'city.required' => 'El campo Ciudad es obligatorio.',
-            'affiliate_time.required' => 'El campo Ciudad es obligatorio.',
-
+            'affiliate_time.required' => 'El campo Tiempo afiliado es obligatorio.',
+            'email.required' => 'El campo Correo electrónico es obligatorio.',
+            'email.email' => 'El Correo electrónico debe ser una dirección de correo electrónico válida.',
+            'phone.required' => 'El campo Teléfono es obligatorio.',
+            'mobile.required' => 'El campo Celular es obligatorio.',
         ];
     }
 
